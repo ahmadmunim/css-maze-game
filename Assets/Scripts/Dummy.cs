48919a080540031d0f1b4d716f94f4d2da1aedf3
Cyoger <akshat1sharma2@gmail.com>
2023-07-27 00:15:33 -0400

    beep boop

---
+++ Assets/Scripts/Dummy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dummy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+    
+
+    void start() {
+    }
+
+    void Update()
+    {
+        // Calculate the movement amount based on the prefab's forward direction
+        Vector3 moveAmount = transform.forward * moveSpeed * Time.deltaTime;
+
+        // Move the prefab
+        transform.Translate(moveAmount, Space.World);
+    }
+}
